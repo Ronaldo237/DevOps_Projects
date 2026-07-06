471f2af210b4f42cf7ce5520bbda634647264fab
Ronaldo237 <jasondongmo2003@gmail.com>
2026-07-06 22:31:00 +0200

    test pipline

---
+++ deploy_aws/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+app = express()
+
+app.get("/", (req, res) => {
+    res.send("Service is up and running!")
+})
+
+
+app.listen(8080, () =>{
+    console.log("Server is running on port 8080")
+})
